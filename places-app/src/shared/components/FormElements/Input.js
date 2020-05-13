675926d2ebd4d67872fd29937ffd82215f78d8ec
Jortega23 <jortegs23@gmail.com>
2020-05-12 20:21:06 -0700

    WIP
    FInishing up form

--- places-app/src/shared/components/FormElements/Input.js
+++ places-app/src/shared/components/FormElements/Input.js
@@ -1,4 +1,4 @@
-import React, {useReducer} from 'react'
+import React, {useReducer, useEffect} from 'react'
 
 import { validate } from '../../util/validators'
 import './Input.css'
@@ -27,6 +27,12 @@ const inputReducer = (state, action) => {
 const Input = props => {
   const [inputState, dispatch] =  useReducer(inputReducer, {value: '', isValid: false, isTouched: false});
 
+    const { id, onInput } = props;
+    const { value, isValid } = inputState;
+  
+    useEffect(() => {
+        onInput(id, value, isValid)
+    }, [id, value, isValid, onInput]);
 
 
     const changeHandler = event => {
